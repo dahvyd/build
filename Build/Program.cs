48ec014cf19d4983d380d2b78b54d53659087cc1
Dave East <dahvyd@gmail.com>
2011-12-20 15:35:23 +1100

    initial commit

---
+++ Build/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+
+namespace Build
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = args.Length > 0 ? args[0] : GetFilePathFromConsole();
+			if (Directory.Exists(filePath))
+			{
+				new Builder().Build(filePath);
+			}
+			else
+			{
+				Console.WriteLine("Couldn't find build path {0}, exiting.", filePath);
+			}
+		}
+
+		static string GetFilePathFromConsole()
+		{
+			Console.Write("Enter a build target file path: ");
+			return Console.ReadLine();
+		}
+	}
+}
